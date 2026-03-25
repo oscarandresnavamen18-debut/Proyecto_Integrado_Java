9b2902014410fb8ef66f40bc594751771bd5f5f3
Oscar Navarro <oscarandresnavamen18@gmail.com>
2026-03-25 09:44:17 -0500

    Subo Clase Herencia de Java

---
+++ src/main/java/com/example/Convertor.java
@@ -0,0 +1,82 @@
+package com.example;
+
+public abstract class Convertor {
+    private String nombreCategoria;
+    private String icono;
+    private double valorEntrada;
+    private String unidadOrigen;
+    private String unidadDestino;
+    private double resultado;
+   
+   
+    public Convertor(String nombreCategoria, String icono, double valorEntrada, String unidadOrigen,
+        String unidadDestino, double resultado) {
+        this.nombreCategoria = nombreCategoria;
+        this.icono = icono;
+        this.valorEntrada = valorEntrada;
+        this.unidadOrigen = unidadOrigen;
+        this.unidadDestino = unidadDestino;
+        this.resultado = resultado;
+    }
+
+
+    public String getNombreCategoria() {
+        return nombreCategoria;
+    }
+
+
+    public void setNombreCategoria(String nombreCategoria) {
+        this.nombreCategoria = nombreCategoria;
+    }
+
+
+    public String getIcono() {
+        return icono;
+    }
+
+
+    public void setIcono(String icono) {
+        this.icono = icono;
+    }
+
+
+    public double getValorEntrada() {
+        return valorEntrada;
+    }
+
+
+    public void setValorEntrada(double valorEntrada) {
+        this.valorEntrada = valorEntrada;
+    }
+
+
+    public String getUnidadOrigen() {
+        return unidadOrigen;
+    }
+
+
+    public void setUnidadOrigen(String unidadOrigen) {
+        this.unidadOrigen = unidadOrigen;
+    }
+
+
+    public String getUnidadDestino() {
+        return unidadDestino;
+    }
+
+
+    public void setUnidadDestino(String unidadDestino) {
+        this.unidadDestino = unidadDestino;
+    }
+
+
+    public double getResultado() {
+        return resultado;
+    }
+
+
+    public void setResultado(double resultado) {
+        this.resultado = resultado;
+    }
+    
+}
--- src/main/java/com/example/application/views/inicio/InicioView.java
+++ src/main/java/com/example/application/views/inicio/InicioView.java
@@ -18,14 +18,16 @@ public class InicioView extends VerticalLayout {
     public InicioView() {
         setSpacing(false);
 
-        Image img = new Image("images/empty-plant.png", "placeholder plant");
+        Image img = new Image(
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHefDutjw2ztSQwW4UXCziu4lCYtmsZH6aHQ&s",
+                "placeholder plant");
         img.setWidth("200px");
         add(img);
 
-        H2 header = new H2("This place intentionally left empty");
+        H2 header = new H2("Bienvenidos a Nuestro convertor");
         header.addClassNames(Margin.Top.XLARGE, Margin.Bottom.MEDIUM);
         add(header);
-        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+        add(new Paragraph("En este Conveertor vamos a encontrar multiples Cosas.😊"));
 
         setSizeFull();
         setJustifyContentMode(JustifyContentMode.CENTER);
